8896fbbed898214bfc6c096f60a4ee48833b9151
duyanh-y4n <duyanh.y4n.pham@gmail.com>
2020-09-11 20:45:10 +0200

    add Sysview option

--- custom/debug_output.h
+++ custom/debug_output.h
@@ -7,13 +7,22 @@
 #ifndef DEBUG_OUTPUT_H
 #define DEBUG_OUTPUT_H
 
+/*
+ * SEMIHOSTING_ACTIVE
+ * */
 #if defined(SEMIHOSTING_ACTIVE)
 #define debug_printf(x, y, ...) ((x) ? trace_printf(y, ##__VA_ARGS__) : 0)
 
+/*
+ * SYSVIEWPRINT_ACTIVE
+ * */
 #elif defined(SYSVIEWPRINT_ACTIVE)
 #define debug_printf(x, y, ...)                                                \
   ((x) ? SEGGER_SYSVIEW_printf(0, y, ##__VA_ARGS__) : 0)
 
+/*
+ * RTT_ACTIVE
+ * */
 #elif defined(RTT_ACTIVE)
 #include "SEGGER_RTT.h"
 
@@ -29,6 +38,22 @@
 #define INFO(x, y, ...) ((x) ? SEGGER_RTT_printf(0, y, ##__VA_ARGS__) : 0)
 
 #define ERROR(y, ...) SEGGER_RTT_printf(0, y, ##__VA_ARGS__)
+
+/*
+ * -DEBUG_OUTPUT disable
+ * */
+#else
+#define RTT_Init() (0)
+
+#define debug_printf(x, y, ...) (0)
+
+#undef TRACE // resolve warning
+
+#define TRACE(x, y, ...) (0)
+
+#define INFO(x, y, ...) (0)
+
+#define ERROR(y, ...) (0)
 #endif
 
 #endif /* DEBUG_OUTPUT_H */
